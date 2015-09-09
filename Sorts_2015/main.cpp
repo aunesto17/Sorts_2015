6215bf5f5f6962e6b10bb7c283ae8cff9aa074c4
Alex Baylon <aunesto15@gmail.com>
2015-09-08 22:38:43 -0500

    Initial Commit

---
+++ Sorts_2015/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sorts_2015
+//
+//  Created by Alexander Arturo Baylon Ibanez on 8/09/15.
+//  Copyright (c) 2015 Alexander Arturo Baylon Ibanez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
